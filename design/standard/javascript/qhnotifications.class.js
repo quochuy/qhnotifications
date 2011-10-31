0bf8b878133284211543fd45f439eabfb3baee33
NGUYEN DINH Quoc-Huy <contact@quoc-huy.com>
2011-11-01 00:45:42 +0100

    When more than two notifications, only displays two of them and add an 'expand' button

--- design/standard/javascript/qhnotifications.class.js
+++ design/standard/javascript/qhnotifications.class.js
@@ -1,22 +1,55 @@
+// jQuery doesn't see the attributes and methods of QHNotifications class from a callback
+// So we put them here
+var QHNotificationsFunctions = {
+    // Displays or hide the expand button
+    toggleExpandButton: function() {
+        if( this.numberOfNotifications() > 2 ) {
+            $( '#notification-container-expand' ).css( 'display', 'block' );
+        } else {
+            $( '#notification-container-expand' ).css( {'display': 'none', 'margin-top': 0} );
+            this.setExpandAction( 'Expand' );
+        }
+    },
+
+    // Retreives the number of notifications
+    numberOfNotifications: function() {
+        return $( '.notification-item' ).length;
+    },
+
+    // Retreive the current action if the expand button is clicked
+    getExpandAction: function() {
+        return $( '#expand-action' ).html();
+    },
+
+    // Set the current action if the expand button is clicked
+    setExpandAction: function( action ) {
+        $( '#expand-action' ).html( action );
+    }
+
+}
+
+// Main notification class
 function QHNotifications( class_parms ) {
-    /* Classes attributes */
-    this.configs = { notification_height: 15, max_notification_items: 3 };
+    // Classes attributes
     this.notification_array = new Array();
 
-    /* Constructor */
+    // Constructor
     this.init = function( class_parms ) {
         this.injectContainer();    
     }
 
-    /* Injects the DIV container HTML code into the page */
+    // Injects the DIV container HTML code into the page
     this.injectContainer = function() {
         // Injects the container only if not already injected
         if( $( '#notification-container' ).length == 0 ) {
-            $( 'body' ).prepend( '<div id="notification-container"></div>' );        
+            $( 'body' ).prepend( '<div id="notification-container"></div><div id="notification-container-expand"><div id="notification-container-expand-button"><span id="expand-action">Expand</span></div></div>' );
+            
+            // Binds the click event to the expand button
+            $( '#notification-container-expand-button' ).bind( 'click', this.expandCollapse );
         }
     }
 
-    /* Inserts a new message in the queue */
+    // Inserts a new message in the queue
     this.setMessage = function( parms ) {
         var foundID = false;
         // To avoid inserting the same message twice, search and replace message if already in the queue
@@ -31,58 +64,87 @@ function QHNotifications( class_parms ) {
             this.notification_array.unshift( parms ); 
     }
 
-    /* Inserts and displays notification items */
+    // Inserts and displays notification items
     this.display = function() {
         var page_offset = 0;
+        var expand_action = QHNotificationsFunctions.getExpandAction();
+
         // Displays all notification items
-        var displayedItems = 0;
         for( var i=0; i<this.notification_array.length; i++ ) {
             var notification_item = this.notification_array[i];
 
             // If a notification item is not already on screen, insert it
             if( $( '#'+ notification_item.notification_id ).length == 0 ) {
-                displayedItems++;
                 $( '#notification-container' ).prepend( '<div class="notification-item" id="'+ notification_item.notification_id +'">' +
                                                         '   <span class="notification_text">'+ notification_item.text +'</span>' +
                                                         '</div>'
                 );
 
+                // When a notification is not 'sticky' display a removal button
                 if( typeof notification_item.type == 'undefined' || notification_item.type != 'sticky' ) {
                     $( '#'+ notification_item.notification_id ).append( '<div class="notification_close_button"><span>[X]</span></div>' );
                     $( '#'+ notification_item.notification_id ).bind( 'click', {notification_id: notification_item.notification_id}, this.removeNotification );
                 }
 
-                $( '#'+ notification_item.notification_id ).css( 'height', this.configs.notification_height ); 
+                // Updates the page's main content top margin value
+                page_offset = page_offset + parseInt( $( '#'+ notification_item.notification_id ).outerHeight() );
 
-                page_offset = page_offset + parseInt( $( '#'+ notification_item.notification_id ).outerHeight() );
+            // Else update it
             } else {
-                // Else update it
-                $( '#'+ notification_item.notification_id +' .notification_text' ).html(notification_item.text);
-                page_offset = page_offset + parseInt( $( '#'+ notification_item.notification_id ).outerHeight() );
+                $( '#'+ notification_item.notification_id +' .notification_text' ).html( notification_item.text );
+                page_offset = page_offset + parseInt( $( '.notification-item:first' ).outerHeight() );
             }
+
+            if( QHNotificationsFunctions.numberOfNotifications() > 2 ) {
+                // If the current expand action is 'Expand' then set the notification container height two the height of two items  
+                if( expand_action == 'Expand' ) {
+                    $( '#notification-container' ).css( 'height', parseInt( 2 * $( '#'+ notification_item.notification_id ).outerHeight() ) );
+                }
+                page_offset = 2 * parseInt( $( '.notification-item:first' ).outerHeight() );
+            }
+
+            QHNotificationsFunctions.toggleExpandButton();
 
         }
 
         $( '#page' ).css( 'margin-top', page_offset );
     }
 
-    /* Removes any non sticky notifications */
+    // Call back function for the expand button
+    this.expandCollapse = function() {
+        var action = QHNotificationsFunctions.getExpandAction();
+
+        var number_of_items = QHNotificationsFunctions.numberOfNotifications();
+        var item_height = $( '.notification-item:last' ).outerHeight();
+
+        if( action == 'Expand' ) {
+            var container_height = number_of_items * item_height;
+            var expand_top = (number_of_items - 2 ) * item_height;
+            QHNotificationsFunctions.setExpandAction( 'Collapse' );
+        } else {
+            var container_height = 2 * item_height;
+            var expand_top = 0;
+            QHNotificationsFunctions.setExpandAction('Expand' );
+        }
+
+        // Animate the expand/collapse action
+        $( '#notification-container' ).animate({'height': container_height}, 500);
+        $( '#notification-container-expand' ).animate({'margin-top': expand_top}, 500);
+    }
+
+    // Removes any non sticky notifications
     this.removeNotification = function( parm ) {
         // calculates the new top margin for the page content
-        this.page_offset = parseInt( $( '#page' ).css( 'margin-top' ) ) - parseInt( $( '#'+ parm.data.notification_id ).outerHeight() );
+        page_offset = parseInt( $( '#page' ).css( 'margin-top' ) ) - parseInt( $( '#'+ parm.data.notification_id ).outerHeight() );
 
         // Removing animation
         $( '#'+ parm.data.notification_id ).animate( {'height': 0}, 500, 'swing', function() {
             $( '#'+ parm.data.notification_id ).remove();
+            QHNotificationsFunctions.toggleExpandButton();
         });
 
         // Page content re-adjustment animation
-        $( '#page' ).animate( {'margin-top': this.page_offset}, 500 );
-    }
-
-    /* Using Ajax to load eZ INI config file */
-    this.getConfigByAjaxCall = function() {
-
+        $( '#page' ).animate( {'margin-top': page_offset}, 500 );
     }
 
     this.init( class_parms );
